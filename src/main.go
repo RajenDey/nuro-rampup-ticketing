90f73a9785caaf25ab877229926d3768c0e5d8e4
Rajen Dey <rajen.dey@berkeley.edu>
2022-04-13 22:05:59 -0700

    do the rest samvit

--- src/main.go
+++ src/main.go
@@ -13,7 +13,7 @@ import (
 	"google.golang.org/grpc"
 	"log"
 	"net"
-	"nuro-rampup-ticketing/database/models"
+	// "nuro-rampup-ticketing/database/models"
 	pb "nuro-rampup-ticketing/proto"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -34,6 +34,29 @@ func (s *server) Test(ctx context.Context, in *pb.TestReq) (*pb.TestRes, error) 
 
 // TODO: step 5. Write controllers for your endpoints that you defined in `pokemon.proto`
 
+func (s *server) CreatePokemon(ctx context.Context, in *pb.CreatePokemonReq) (*pb.CreatePokemonRes, error) {
+	pokemon := in.GetPokemon()
+	db_global.Exec("INSERT INTO pokemon (Type, Name, Points, ID) VALUES (?, ?, ?, ?)", pokemon.Type, pokemon.Name, pokemon.Points, pokemon.ID)
+	return &pb.CreatePokemonRes{Success: true}, nil
+}
+
+func (s *server) ReadPokemon(ctx context.Context, in *pb.ReadPokemonReq) (*pb.ReadPokemonRes, error) {
+	id := in.GetID()
+	row := db_global.QueryRow("SELECT * FROM pokemon WHERE ID = ?", id)
+	var type_ string
+	var name string
+	var points int32
+	var id_ int32
+	row.Scan(&type_, &name, &points, &id_)
+	return &pb.ReadPokemonRes{Pokemon: &pb.Pokemon{Type: type_, Name: name, Points: points, ID: id_}}, nil
+}
+
+func (s *server) UpdatePokemon(ctx context.Context, in *pb.UpdatePokemonReq) (*pb.UpdatePokemonRes, error) {
+	ID := in.GetID()
+	Points := in.GetPoints()
+	db_global.Exec("UPDATE pokemon SET Points = ? WHERE ID = ?", Points, ID)
+	return &pb.UpdatePokemonRes{Success: true}, nil
+}
 
 // Starts the Server
 func main() {
